05f51e710ba9500e5391751930ff296648bf1db6
Ayaz AA <ayazavzalov77@gmail.com>
2021-02-27 23:36:29 +0300

    4) Создан js код по переключению между вкладками при помощи цикла

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+var main = function () {
+    "use strict";
+    var tabNumber;
+    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(tabSelector).on("click", function () {
+            $(".tabs span").removeClass("active");
+            $(this).addClass("active");
+            $("main.content").empty();
+            return false;
+        });
+    }
+}
+$(document).ready(main);
