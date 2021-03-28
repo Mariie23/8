2d3c44972cea152a98dff7d870414d806dc57c3a
Ayaz AA <ayazavzalov77@gmail.com>
2021-03-28 17:15:13 +0300

    8) Добавлена реализация функций index, create в контролер задач

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,10 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+    ObjectId = mongoose.Schema.Types.ObjectId;
 
 var ToDoSchema = mongoose.Schema({
     description: String,
-    tags: [String]
+    tags: [String],
+    owner: { type: ObjectId, ref: "User" }
 });
 var ToDo = mongoose.model("ToDo", ToDoSchema);
-
 module.exports = ToDo;
